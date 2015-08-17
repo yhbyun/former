367b3747321556428ba9cf878548326e57f620d5
YongHun Byun <yonghunbyun@gmail.com>
2015-08-17 12:18:54 +0900

    Merge TwitterBootstrap3Validator configurations into master config file

--- src/config/former.php
+++ src/config/former.php
@@ -90,6 +90,29 @@
 
 	),
 
+	'TwitterBootstrap3Validator'       => array(
+
+		// Map Former-supported viewports to Bootstrap 3 equivalents
+		'viewports'   => array(
+			'large'  => 'lg',
+			'medium' => 'md',
+			'small'  => 'sm',
+			'mini'   => 'xs',
+		),
+		// Width of labels for horizontal forms expressed as viewport => grid columns
+		'labelWidths' => array(
+			'large' => 2,
+			'small' => 4,
+		),
+		// HTML markup and classes used by Bootstrap 3 for icons
+		'icon'        => array(
+			'tag'    => 'span',
+			'set'    => 'glyphicon',
+			'prefix' => 'glyphicon',
+		),
+
+	),
+
 	'Nude'                    => array(  // No-framework markup
 		'icon' => array(
 			'tag'    => 'i',
